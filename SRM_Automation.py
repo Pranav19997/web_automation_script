19d0535159ead5c8f720ebac7e22b5f208580591
Pranav19997 <41124463+Pranav19997@users.noreply.github.com>
2018-07-16 18:53:19 +0530

    Update SRM_Automation.py

--- SRM_Automation.py
+++ SRM_Automation.py
@@ -6,12 +6,26 @@ rows=worksheet.nrows
 columns=worksheet.ncols                             #to access the number of rows and columns
 row_data=list()                                     #creating lists for data storage
 column_data=list()
+sub_code=list()
 for y in range(columns):                            #for loop to access the first row
     row_data.append(worksheet.cell(0,y).value)
     if worksheet.cell(0,y).value=="Market":
         column_num=y
 for x in range(rows):
-    row_data.append(worksheet.cell(x,column_num).value)
-print(row_data)
-print(column_data)
+    column_data.append(worksheet.cell(x,column_num).value)
+
+for y in range(columns):
+     var=str(worksheet.cell(1,y).value)
+     m=var[0]+var[1]
+     if var.isalnum()==True:                        #to check for alphanumeric strings
+         if m=="EE" or m=="15":                     #additional test routine to optimize the process
+             sub_code.append(var)
+        
+
+print(row_data)                                     #to output specific row data
+print(column_data)                                  #to output specific column data
+print(sub_code)                                     #to output specified subject code
+
+  
+
 #Pranav Code
